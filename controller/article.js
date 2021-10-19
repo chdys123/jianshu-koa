9432d08608180f110c1b355d2dfc4d555f9343c1
ch123wly <2645133404@qq.com>
2021-10-19 19:28:37 +0800

    master

--- controller/article.js
+++ controller/article.js
@@ -141,6 +141,8 @@ const update = async ctx => {
                 err
             }
         })
+        // 140.82.113.4 github.com
+        // 199.232.69.194 github.global.ssl.fastly.net
 }
 
 module.exports = {
